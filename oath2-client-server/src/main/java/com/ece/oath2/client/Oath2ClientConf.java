51205b7fb4afa1fc7f363e7fb7401fd78436b3db
NagendrappaE <40258984+NagendrappaE@users.noreply.github.com>
2025-10-03 09:23:20 +0530

    Update Oath2ClientConf.java

--- oath2-client-server/src/main/java/com/ece/oath2/client/Oath2ClientConf.java
+++ oath2-client-server/src/main/java/com/ece/oath2/client/Oath2ClientConf.java
@@ -1,36 +1,16 @@
 package com.ece.oath2.client;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
-import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.UnrecoverableKeyException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Base64;
 
-import javax.net.ssl.SSLContext;
-import javax.security.cert.CertificateException;
-
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.io.HttpClientConnectionManager;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
@@ -69,40 +49,44 @@ public class Oath2ClientConf {
 		// Cast to RSAPrivateKey
 		return (RSAPrivateKey) privateKey;
 	}
-	
-	
 
-	@Value("${trust.store}")
-	private Resource trustStore;
-
-	@Value("${trust.store.password}")
-	private String trustStorePassword;
-	
-
-
+	/*
+	 * @Value("${trust.store}") private Resource trustStore;
+	 * 
+	 * @Value("${trust.store.password}") private String trustStorePassword;
+	 */
 	@Bean
-	public RestTemplate restTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException,
-			CertificateException, MalformedURLException, IOException, java.security.cert.CertificateException, UnrecoverableKeyException {
+	public RestTemplate restTemplate() {
 
-		KeyStore keyStore = KeyStore.getInstance("PKCS12");
-	    try (FileInputStream fis = new FileInputStream("/home/nagendrappae/Documents/openFinance/altareqKeystore.jks")) {
-	        keyStore.load(fis, "Flux@123".toCharArray());
-	    }
-	    
-		SSLContext sslContext = new SSLContextBuilder()
-				.loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
-				.loadKeyMaterial(keyStore, "Flux@123".toCharArray())
-				.build();
-		
-		
-
-	    
-	    
-		SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
-		HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
-				.setSSLSocketFactory(sslConFactory).build();
-		CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
-		ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-		return new RestTemplate(requestFactory);
+		return new RestTemplate();
 	}
+
+	/*
+	 * @Bean public RestTemplate restTemplate() throws KeyManagementException,
+	 * NoSuchAlgorithmException, KeyStoreException, CertificateException,
+	 * MalformedURLException, IOException, java.security.cert.CertificateException,
+	 * UnrecoverableKeyException {
+	 * 
+	 * KeyStore keyStore = KeyStore.getInstance("PKCS12"); try (FileInputStream fis
+	 * = new FileInputStream(
+	 * "/home/nagendrappae/Documents/openFinance/altareqKeystore.jks")) {
+	 * keyStore.load(fis, "Flux@123".toCharArray()); }
+	 * 
+	 * SSLContext sslContext = new SSLContextBuilder()
+	 * .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
+	 * .loadKeyMaterial(keyStore, "Flux@123".toCharArray()) .build();
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * SSLConnectionSocketFactory sslConFactory = new
+	 * SSLConnectionSocketFactory(sslContext); HttpClientConnectionManager cm =
+	 * PoolingHttpClientConnectionManagerBuilder.create()
+	 * .setSSLSocketFactory(sslConFactory).build(); CloseableHttpClient httpClient =
+	 * HttpClients.custom().setConnectionManager(cm).build();
+	 * ClientHttpRequestFactory requestFactory = new
+	 * HttpComponentsClientHttpRequestFactory(httpClient); return new
+	 * RestTemplate(requestFactory); }
+	 */
 }
